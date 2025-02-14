b459c5ba0fad563dde799b030fc16df3b311c96d
Ankur Halder <ankur.halder12345@gmail.com>
2025-02-15 00:09:52 +0530

    update app title and class name for improved branding

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import TodoList from "./components/TodoList";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Todo List with Redux and Local Storage</h1>
+    <div className="app">
+      <h1>MyToodle</h1>
       <TodoForm />
       <TodoList />
     </div>
